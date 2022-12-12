5f9bbe79435390c9d788e361c49238d8fa2c6556
ionite34 <dev@ionite.io>
2022-12-13 01:51:05 +0800

    Removed test demos

--- _demos/_main2.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def foo():
-    return "abc 123 def"
-print(sys.getrefcount(foo()))
-
-s2 = "500 def 123"
-print(sys.getrefcount(s2))
--- _demos/_main3.py
+++
@@ -1,7 +0,0 @@
-from types import CodeType
-
-import _main2
-import dis
-
-co: CodeType = _main2.__spec__.loader.get_code("_main2")
-print(co.co_consts)
