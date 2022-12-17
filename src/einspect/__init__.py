7de6a1827e43af38361e27f2c718efc8e6f0e584
ionite34 <dev@ionite.io>
2022-12-17 11:03:21 +0800

    Fix unsafe context class call

--- src/einspect/__init__.py
+++ src/einspect/__init__.py
@@ -1,2 +1,6 @@
 from einspect.views.factory import view
-from einspect.views.unsafe import unsafe
+from einspect.views import unsafe as _unsafe
+
+__all__ = ["view", "unsafe"]
+
+unsafe = _unsafe.Context.unsafe
