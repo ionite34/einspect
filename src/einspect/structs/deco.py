b492aa40030ec168103978e04dd2cc5f5fc6ce21
ionite34 <dev@ionite.io>
2022-12-21 17:33:52 +0800

    Add verbose error for fields set failure

--- src/einspect/structs/deco.py
+++ src/einspect/structs/deco.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
-from ctypes import Structure
+from ctypes import Structure, py_object
 from typing import Callable, Type, TypeVar, get_type_hints
 
+
 from einspect.api import Py_ssize_t
+from einspect.protocols.type_parse import is_ctypes_type
 
 _T = TypeVar("_T", bound=Type[Structure])
 
@@ -30,6 +32,10 @@ def struct(cls: _T) -> _T:
         if type_hint in types_map:
             type_hint = types_map[type_hint]
 
+        # For non-ctypes types, substitute with py_object
+        if not is_ctypes_type(type_hint):
+            type_hint = py_object
+
         # Check if there is a real class-attribute assigned
         if hasattr(cls, name):
             value = getattr(cls, name)
@@ -40,6 +46,11 @@ def struct(cls: _T) -> _T:
             fields.append((name, type_hint))
 
     # We have to only set this once as _fields_ is final
-    cls._fields_ = fields
+    try:
+        cls._fields_ = fields
+    except TypeError as e:
+        raise TypeError(
+            f"Failed to set _fields_ on {cls.__name__}: {fields}"
+        ) from e
 
     return cls
