dd1f889cd0edce41373c6301e30c63c1c023aad9
ionite34 <dev@ionite.io>
2022-12-21 17:30:46 +0800

    Refactor export ordering

--- src/einspect/__init__.py
+++ src/einspect/__init__.py
@@ -1,8 +1,13 @@
+from __future__ import annotations
+
 from typing import ContextManager
 
-from einspect.views import unsafe as _unsafe
 from einspect.views.factory import view
+from einspect.views.unsafe import Context
+from einspect.views.view_base import View
 
 __all__ = ["view", "unsafe"]
 
-unsafe: ContextManager[None] = _unsafe.Context.unsafe
+unsafe: ContextManager[None] = Context.unsafe
+
+
