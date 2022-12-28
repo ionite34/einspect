d5f67d71294db3efc75181b2de530d82c39c1081
ionite34 <dev@ionite.io>
2022-12-28 11:11:40 +0800

    Unify deco to use type_parse apis

--- src/einspect/structs/deco.py
+++ src/einspect/structs/deco.py
@@ -1,24 +1,27 @@
 from __future__ import annotations
 
-from ctypes import Structure, py_object
+import logging
+from ctypes import Structure
 from typing import Callable, Type, TypeVar, get_type_hints
 
+from einspect.protocols.type_parse import convert_type_hints, fix_ctypes_generics
 
-from einspect.api import Py_ssize_t
-from einspect.protocols.type_parse import is_ctypes_type
+log = logging.getLogger(__name__)
 
 _T = TypeVar("_T", bound=Type[Structure])
-
-# Map of types hints that will be converted by @struct
-types_map = {
-    int: Py_ssize_t,
-}
 
 
 def struct(cls: _T) -> _T:
     """Decorator to declare _fields_ on Structures via type hints."""
     fields = []
-    for name, type_hint in get_type_hints(cls).items():
+    try:
+        hints = get_type_hints(cls)
+    except TypeError:
+        # Normalize annotations of py_object subscripts
+        fix_ctypes_generics(cls.__annotations__)
+        hints = get_type_hints(cls)
+
+    for name, type_hint in hints.items():
         # Skip actual values like _fields_
         if name.startswith("_") and name.endswith("_"):
             continue
@@ -28,13 +31,8 @@ def struct(cls: _T) -> _T:
         # Since get_type_hints also gets superclass hints, skip them
         if name not in cls.__annotations__:
             continue
-        # Convert type hint if needed
-        if type_hint in types_map:
-            type_hint = types_map[type_hint]
 
-        # For non-ctypes types, substitute with py_object
-        if not is_ctypes_type(type_hint):
-            type_hint = py_object
+        type_hint = convert_type_hints(type_hint, cls)
 
         # Check if there is a real class-attribute assigned
         if hasattr(cls, name):
