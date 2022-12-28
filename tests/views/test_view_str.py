daeb79120d0470cc3face8a63bcb4e1ec95bf210
ionite34 <dev@ionite.io>
2022-12-28 11:09:31 +0800

    Simplify test_view_str tests

--- tests/views/test_view_str.py
+++ tests/views/test_view_str.py
@@ -1,7 +1,3 @@
-from copy import copy
-
-import pytest
-
 from einspect import view, structs
 from einspect.structs.py_unicode import State, Kind
 from einspect.views.view_str import StrView
@@ -9,39 +5,26 @@ from einspect.views.view_str import StrView
 TEXT = "2fe7b604-9664-41e6-9af0-9b472864bfc8"
 
 
-@pytest.fixture(scope="module")
-def str_view():
-    return view(copy(TEXT), ref=True)
-
-
 class TestStrView:
-    def test_type(self, str_view):
-        assert isinstance(str_view, StrView)
-        assert isinstance(str_view._pyobject, structs.PyObject)
+    def setup(self):
+        self.view = view(TEXT, ref=True)
+
+    def test_type(self):
+        assert isinstance(self.view, StrView)
+        assert isinstance(self.view._pyobject, structs.PyObject)
 
     def test_properties(self):
-        x = "hello"
-        x_hash = hash(x)
-        v = view(x)
-        assert type(v.length) == int
-        assert v.length == 5
+        assert self.view.length == len(TEXT)
+        expected_hash = hash(TEXT)
+        assert self.view.hash == expected_hash
+        assert self.view.kind == Kind.PyUnicode_1BYTE
 
-        assert type(v.hash) == int
-        assert v.hash == x_hash
-
-        assert v.kind == Kind.PyUnicode_1BYTE
-
-    def test_intern(self, str_view):
-        # str_view fixture view has no literal refs
-        # so, it should not be interned
-        assert str_view.interned == State.NOT_INTERNED
-
-        # Create a function-level mortal interned string
+    def test_intern(self):
+        # Module level literal should be not interned
+        assert self.view.interned == State.NOT_INTERNED
+        # Create a function-level interned string
         s = "hi"
         assert view(s).interned == State.INTERNED_MORTAL
 
     def test_buffer(self):
-        """Access buffer."""
-        obj = "hello"
-        v = view(obj)
-        assert v.buffer[:] == b"hello"
+        assert self.view.buffer[:] == TEXT.encode("ascii")
