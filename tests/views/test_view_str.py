a7dc0bbde65fbae22f4c717919ea11f44d220d91
ionite34 <dev@ionite.io>
2022-12-28 23:29:06 +0800

    Change setup to fixture

--- tests/views/test_view_str.py
+++ tests/views/test_view_str.py
@@ -1,3 +1,5 @@
+import pytest
+
 from einspect import view, structs
 from einspect.structs.py_unicode import State, Kind
 from einspect.views.view_str import StrView
@@ -5,26 +7,28 @@ from einspect.views.view_str import StrView
 TEXT = "2fe7b604-9664-41e6-9af0-9b472864bfc8"
 
 
+@pytest.fixture(scope="function")
+def str_view() -> StrView:
+    return view(TEXT, ref=True)
+
+
 class TestStrView:
-    def setup(self):
-        self.view = view(TEXT, ref=True)
+    def test_type(self, str_view):
+        assert isinstance(str_view, StrView)
+        assert isinstance(str_view._pyobject, structs.PyObject)
 
-    def test_type(self):
-        assert isinstance(self.view, StrView)
-        assert isinstance(self.view._pyobject, structs.PyObject)
-
-    def test_properties(self):
-        assert self.view.length == len(TEXT)
+    def test_properties(self, str_view):
+        assert str_view.length == len(TEXT)
         expected_hash = hash(TEXT)
-        assert self.view.hash == expected_hash
-        assert self.view.kind == Kind.PyUnicode_1BYTE
+        assert str_view.hash == expected_hash
+        assert str_view.kind == Kind.PyUnicode_1BYTE
 
-    def test_intern(self):
+    def test_intern(self, str_view):
         # Module level literal should be not interned
-        assert self.view.interned == State.NOT_INTERNED
+        assert str_view.interned == State.NOT_INTERNED
         # Create a function-level interned string
         s = "hi"
         assert view(s).interned == State.INTERNED_MORTAL
 
-    def test_buffer(self):
-        assert self.view.buffer[:] == TEXT.encode("ascii")
+    def test_buffer(self, str_view):
+        assert str_view.buffer[:] == TEXT.encode("ascii")
