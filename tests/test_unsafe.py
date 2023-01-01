c2f51680177d1375087f8981cdf33745642e525e
ionite34 <dev@ionite.io>
2023-01-01 14:37:35 +0800

    Simplified test_unsafe scopes

--- tests/test_unsafe.py
+++ tests/test_unsafe.py
@@ -5,19 +5,9 @@ from einspect import view, errors
 
 
 def test_unsafe_error():
-    x = "hello"
-    v = view(x)
-    current = v.hash
+    v = view(1)
+    current = v.ref_count
     with pytest.raises(errors.UnsafeError):
-        v.hash = 500
+        v.ref_count = 0
     # setter should not have run
-    assert v.hash == current
-
-
-def test_tuple_set_item():
-    t = (1, 2, 3)
-    v = view(t)
-    with v.unsafe():
-        v[1] = 4
-        v[2] = 5
-    assert t == (1, 4, 5)
+    assert v.ref_count == current
