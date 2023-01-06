f6f5751cc488829c9048f83e04abb3d46ff74cc1
ionite34 <dev@ionite.io>
2023-01-06 22:12:12 +0800

    Improve typing accuracy for py_set

--- src/einspect/structs/py_set.py
+++ src/einspect/structs/py_set.py
@@ -1,38 +1,44 @@
 from __future__ import annotations
 
-from ctypes import Structure, POINTER
+from ctypes import Structure, POINTER, pointer, Array
+from typing import TypeVar, Generic
 
 from typing_extensions import Annotated
 
+from einspect.types import ptr
 from einspect.api import Py_hash_t
 from einspect.structs.deco import struct
 from einspect.structs.py_object import PyObject
 
-
+_T = TypeVar("_T")
 PySet_MINSIZE = 8
 """https://github.com/python/cpython/blob/3.11/Include/cpython/setobject.h#L18"""
 
 
 @struct
-class SetEntry(Structure):
-    key: POINTER(PyObject)
+class SetEntry(Structure, Generic[_T]):
+    key: pointer[PyObject[_T, None, None]]
     hash: Annotated[int, Py_hash_t]
 
 
 @struct
-class PySetObject(PyObject):
+class PySetObject(PyObject[set, None, _T]):
     """
     Defines a PySetObject Structure.
 
     https://github.com/python/cpython/blob/3.11/Include/cpython/setobject.h#L36-L59
     """
 
-    fill: int
-    used: int
-    mask: int
-    table: POINTER(SetEntry)
+    fill: int  # Number active and dummy entries
+    used: int  # Number active entries
+    mask: int  # The table contains mask + 1 slots
+    table: ptr[SetEntry[_T]]
     hash: Annotated[int, Py_hash_t]
     finger: int
-    smalltable: SetEntry * PySet_MINSIZE
-    weakreflist: POINTER(PyObject)
+    smalltable: Annotated[Array[SetEntry[_T]], SetEntry * PySet_MINSIZE]
+    weakreflist: ptr[PyObject]
+
+    @classmethod
+    def from_object(cls, obj: set[_T]) -> PySetObject[_T]:
+        return cls.from_address(id(obj))
 
