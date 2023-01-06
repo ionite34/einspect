51e5fd969d65dab7f0e4f35198f80aab671a1cb5
ionite34 <dev@ionite.io>
2023-01-06 12:52:46 +0800

    Simply address check to use pyobject property

--- src/einspect/views/view_bool.py
+++ src/einspect/views/view_bool.py
@@ -1,9 +1,5 @@
 from __future__ import annotations
 
-from ctypes import Array, c_uint32, addressof
-from typing import Iterable, TypeVar
-
-from einspect.api import Py_ssize_t
 from einspect.structs import PyBoolObject
 from einspect.utils import address
 from einspect.views.view_int import IntView
@@ -17,7 +13,7 @@ class BoolView(IntView):
     @property
     def mem_size(self) -> int:
         # If used on the singletons, use their
-        addr = addressof(self._pyobject)
+        addr = self._pyobject.address
         if addr == address(True):
             return True.__sizeof__()
         elif addr == address(False):
