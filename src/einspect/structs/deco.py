a32b61187de1b56e0a5181f87307e4ebcb66a1fd
ionite34 <dev@ionite.io>
2023-01-06 09:39:56 +0800

    Fix Annotated resolution

--- src/einspect/structs/deco.py
+++ src/einspect/structs/deco.py
@@ -4,7 +4,8 @@ import logging
 from ctypes import Structure
 from typing import Callable, Type, TypeVar, get_type_hints, get_args
 
-from typing_extensions import Annotated
+# noinspection PyUnresolvedReferences, PyProtectedMember
+from typing_extensions import _AnnotatedAlias
 
 from einspect.protocols.type_parse import convert_type_hints, fix_ctypes_generics
 
@@ -17,11 +18,11 @@ def struct(cls: _T) -> _T:
     """Decorator to declare _fields_ on Structures via type hints."""
     fields = []
     try:
-        hints = get_type_hints(cls)
+        hints = get_type_hints(cls, include_extras=True)
     except TypeError:
         # Normalize annotations of py_object subscripts
         fix_ctypes_generics(cls.__annotations__)
-        hints = get_type_hints(cls)
+        hints = get_type_hints(cls, include_extras=True)
 
     for name, type_hint in hints.items():
         # Skip actual values like _fields_
@@ -35,9 +36,12 @@ def struct(cls: _T) -> _T:
             continue
 
         # For Annotated, directly use fields 1 and 2
-        if type(type_hint) is Annotated:
+        if type(type_hint) is _AnnotatedAlias:
             args = get_args(type_hint)
+            res = (name, *args[1:3])
+            log.debug(f"Annotated: {type_hint} -> {res}")
             fields.append((name, *args[1:3]))
+            continue
 
         type_hint = convert_type_hints(type_hint, cls)
         fields.append((name, type_hint))
