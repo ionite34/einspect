63f517c0b54507cf045a692a172d6727cd850907
ionite34 <dev@ionite.io>
2023-01-09 20:49:03 +0800

    Add TpFlags Enum

--- src/einspect/structs/include/object_h.py
+++ src/einspect/structs/include/object_h.py
@@ -38,80 +38,53 @@ __all__ = (
     "allocfunc",
     "vectorcallfunc",
     "sendfunc",
+    "PySendResult",
+    "TpFlags",
     "PyAsyncMethods",
     "PyNumberMethods",
     "PyMappingMethods",
     "PySequenceMethods",
 )
 
-
 # Function types
 # https://github.com/python/cpython/blob/3.11/Include/object.h#L196-L227
-
-# typedef PyObject * (*unaryfunc)(PyObject *);
 unaryfunc = PYFUNCTYPE(py_object, py_object)
-# typedef PyObject * (*binaryfunc)(PyObject *, PyObject *);
 binaryfunc = PYFUNCTYPE(py_object, py_object, py_object)
-# typedef PyObject * (*ternaryfunc)(PyObject *, PyObject *, PyObject *);
 ternaryfunc = PYFUNCTYPE(py_object, py_object, py_object, py_object)
-# typedef int (*inquiry)(PyObject *);
 inquiry = PYFUNCTYPE(c_int, py_object)
-# typedef Py_ssize_t (*lenfunc)(PyObject *);
 lenfunc = PYFUNCTYPE(Py_ssize_t, py_object)
-# typedef PyObject *(*ssizeargfunc)(PyObject *, Py_ssize_t);
 ssizeargfunc = PYFUNCTYPE(py_object, py_object, Py_ssize_t)
-# typedef PyObject *(*ssizessizeargfunc)(PyObject *, Py_ssize_t, Py_ssize_t);
 ssizessizeargfunc = PYFUNCTYPE(py_object, py_object, Py_ssize_t, Py_ssize_t)
-# typedef int(*ssizeobjargproc)(PyObject *, Py_ssize_t, PyObject *);
 ssizeobjargproc = PYFUNCTYPE(c_int, py_object, Py_ssize_t, py_object)
-# typedef int(*ssizessizeobjargproc)(PyObject *, Py_ssize_t, Py_ssize_t, PyObject *);
 ssizessizeobjargproc = PYFUNCTYPE(c_int, py_object, Py_ssize_t, Py_ssize_t, py_object)
-# typedef int(*objobjargproc)(PyObject *, PyObject *, PyObject *);
 objobjargproc = PYFUNCTYPE(c_int, py_object, py_object, py_object)
 
-# typedef int (*objobjproc)(PyObject *, PyObject *);
 objobjproc = PYFUNCTYPE(c_int, py_object, py_object)
-# typedef int (*visitproc)(PyObject *, void *);
 visitproc = PYFUNCTYPE(c_int, py_object, c_void_p)
-# typedef int (*traverseproc)(PyObject *, visitproc, void *);
 traverseproc = PYFUNCTYPE(c_int, py_object, visitproc, c_void_p)
 
-# typedef void (*freefunc)(void *);
 freefunc = PYFUNCTYPE(None, c_void_p)
-# typedef void (*destructor)(PyObject *);
 destructor = PYFUNCTYPE(None, py_object)
-# typedef PyObject *(*getattrfunc)(PyObject *, char *);
 getattrfunc = PYFUNCTYPE(py_object, py_object, c_char_p)
-# typedef PyObject *(*getattrofunc)(PyObject *, PyObject *);
 getattrofunc = PYFUNCTYPE(py_object, py_object, py_object)
-# typedef int (*setattrfunc)(PyObject *, char *, PyObject *);
 setattrfunc = PYFUNCTYPE(c_int, py_object, c_char_p, py_object)
-# typedef int (*setattrofunc)(PyObject *, PyObject *, PyObject *);
 setattrofunc = PYFUNCTYPE(c_int, py_object, py_object, py_object)
-# typedef PyObject *(*reprfunc)(PyObject *);
 reprfunc = PYFUNCTYPE(py_object, py_object)
-# typedef Py_hash_t (*hashfunc)(PyObject *);
 hashfunc = PYFUNCTYPE(Py_ssize_t, py_object)
-# typedef PyObject *(*richcmpfunc) (PyObject *, PyObject *, int);
 richcmpfunc = PYFUNCTYPE(py_object, py_object, py_object, c_int)
-# typedef PyObject *(*getiterfunc) (PyObject *);
 getiterfunc = PYFUNCTYPE(py_object, py_object)
-# typedef PyObject *(*iternextfunc) (PyObject *);
 iternextfunc = PYFUNCTYPE(py_object, py_object)
 
-# typedef PyObject *(*descrgetfunc) (PyObject *, PyObject *, PyObject *);
 descrgetfunc = PYFUNCTYPE(py_object, py_object, py_object, py_object)
-# typedef int (*descrsetfunc) (PyObject *, PyObject *, PyObject *);
 descrsetfunc = PYFUNCTYPE(c_int, py_object, py_object, py_object)
-# typedef int (*initproc)(PyObject *, PyObject *, PyObject *);
 initproc = PYFUNCTYPE(c_int, py_object, py_object, py_object)
-# typedef PyObject *(*newfunc)(PyTypeObject *, PyObject *, PyObject *);
 newfunc = PYFUNCTYPE(py_object, py_object, py_object, py_object)
-# typedef PyObject *(*allocfunc)(PyTypeObject *, Py_ssize_t);
 allocfunc = PYFUNCTYPE(py_object, py_object, Py_ssize_t)
 
-# typedef PyObject *(*vectorcallfunc)(PyObject *callable, PyObject *const *args, size_t nargsf, PyObject *kwnames)
+# PyObject *(*vectorcallfunc)(PyObject *callable, PyObject *const *args, size_t nargsf, PyObject *kwnames)
 vectorcallfunc = PYFUNCTYPE(py_object, py_object, ptr[py_object], Py_ssize_t, py_object)
+# PySendResult (*sendfunc)(PyObject *iter, PyObject *value, PyObject **result)
+sendfunc = PYFUNCTYPE(c_int, py_object, py_object, ptr[py_object])
 
 
 class PySendResult(IntEnum):
@@ -122,8 +95,63 @@ class PySendResult(IntEnum):
     PYGEN_NEXT = 1
 
 
-# typedef PySendResult (*sendfunc)(PyObject *iter, PyObject *value, PyObject **result);
-sendfunc = PYFUNCTYPE(c_int, py_object, py_object, ptr[py_object])
+class TpFlags(IntEnum):
+    """
+    Type flags (tp_flags)
+
+    https://github.com/python/cpython/blob/3.11/Include/object.h#L350-L440
+    """
+
+    STATIC_BUILTIN = 1 << 1
+    MANAGED_WEAKREF = 1 << 3
+    MANAGED_DICT = 1 << 4
+    PREHEADER = MANAGED_WEAKREF | MANAGED_DICT
+    # Set if instances of the type object are treated as sequences for pattern matching
+    SEQUENCE = 1 << 5
+    # Set if instances of the type object are treated as mappings for pattern matching
+    MAPPING = 1 << 6
+    # Disallow creating instances of the type: set tp_new to NULL and don't create
+    # the "__new__" key in the type dictionary.
+    DISALLOW_INSTANTIATION = 1 << 7
+    # Set if the type object is immutable: type attributes cannot be set nor deleted
+    IMMUTABLETYPE = 1 << 8
+    # Set if the type object is dynamically allocated
+    HEAPTYPE = 1 << 9
+    # Set if the type allows subclassing
+    BASETYPE = 1 << 10
+    # Set if the type implements the vectorcall protocol (PEP 590)
+    HAVE_VECTORCALL = 1 << 11
+    # Set if the type is 'ready' -- fully initialized
+    READY = 1 << 12
+    # Set while the type is being 'readied', to prevent recursive ready calls
+    READYING = 1 << 13
+    # Objects support garbage collection (see objimpl.h)
+    HAVE_GC = 1 << 14
+    # These two bits are preserved for Stackless Python, next after this is 17
+    HAVE_STACKLESS_EXTENSION = 0  # if STACKLESS (3 << 15)
+    # Objects behave like an unbound method
+    METHOD_DESCRIPTOR = 1 << 17
+    # Object has up-to-date type attribute cache
+    VALID_VERSION_TAG = 1 << 19
+    # Type is abstract and cannot be instantiated
+    IS_ABSTRACT = 1 << 20
+    # This undocumented flag gives certain built-ins their unique pattern-matching
+    # behavior, which allows a single positional subpattern to match against the
+    # subject itself (rather than a mapped attribute on it):
+    MATCH_SELF = 1 << 22
+    # These flags are used to determine if a type is a subclass.
+    LONG_SUBCLASS = 1 << 24
+    LIST_SUBCLASS = 1 << 25
+    TUPLE_SUBCLASS = 1 << 26
+    BYTES_SUBCLASS = 1 << 27
+    UNICODE_SUBCLASS = 1 << 28
+    DICT_SUBCLASS = 1 << 29
+    BASE_EXC_SUBCLASS = 1 << 30
+    TYPE_SUBCLASS = 1 << 31
+    DEFAULT = HAVE_STACKLESS_EXTENSION | 0
+    # Some of these flags reuse lower bits (removed as part of the Python 3.0 transition).
+    HAVE_FINALIZE = 1 << 0
+    HAVE_VERSION_TAG = 1 << 18
 
 
 @struct
