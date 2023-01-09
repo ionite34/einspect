877d8393bf3675ce29be0805326769aa185a29c8
ionite34 <dev@ionite.io>
2023-01-09 08:18:45 +0800

    Increment version to v0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, str(Path(__file__).parents[2] / "src"))
 project = "einspect"
 copyright = "2023, Ionite"
 author = "Ionite"
-release = "v0.3.1"
+release = "v0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
