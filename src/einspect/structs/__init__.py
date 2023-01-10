ec28b8f935bed43ee90526808556ff968b3c8601
ionite34 <dev@ionite.io>
2023-01-10 23:37:56 +0800

    Export PyTypeObject to structs __init__

--- src/einspect/structs/__init__.py
+++ src/einspect/structs/__init__.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
 from einspect.structs.deco import struct
-from einspect.structs.py_object import PyObject, PyVarObject
-from einspect.structs.py_long import PyLongObject
+from einspect.structs.py_object import PyObject, PyVarObject  # Requires import first
+from einspect.structs.py_type import PyTypeObject
+from einspect.structs.py_long import PyLongObject  # Requires import before PyBoolObject
 from einspect.structs.py_bool import PyBoolObject
 from einspect.structs.py_unicode import PyUnicodeObject
 from einspect.structs.py_list import PyListObject
 from einspect.structs.py_tuple import PyTupleObject
 from einspect.structs.py_set import PySetObject
-from einspect.structs.py_dict import PyDictObject
+from einspect.structs.py_dict import (
+    PyDictObject,
+)  # Requires import before PyMappingProxyObject
 from einspect.structs.mapping_proxy import MappingProxyObject
 
 __all__ = (
     "struct",
     "PyObject",
     "PyVarObject",
+    "PyTypeObject",
     "PyTupleObject",
     "PyListObject",
     "PyLongObject",
