1387661f997c057a6442fbb563e826c88de4bacc
ionite34 <dev@ionite.io>
2023-01-10 23:02:53 +0800

    Add PyList display tests

--- tests/views/test_display.py
+++ tests/views/test_display.py
@@ -12,10 +12,25 @@ def test_info_int():
     info = view(x).info()
     assert info == inline(
         f"""
-    PyLongObject (at {hex(id(x))}):
-       ob_refcnt: Py_ssize_t = 3
-       ob_type: *PyTypeObject = &[int]
-       ob_size: Py_ssize_t = 2
-       ob_digit: Array[c_uint32] = [0, 4]
-    """
+        PyLongObject (at {hex(id(x))}):
+           ob_refcnt: Py_ssize_t = 3
+           ob_type: *PyTypeObject = &[int]
+           ob_size: Py_ssize_t = 2
+           ob_digit: Array[c_uint32] = [0, 4]
+        """
+    )
+
+
+def test_info_list():
+    x = [1, "A"]
+    info = view(x).info()
+    assert info == inline(
+        f"""
+        PyListObject (at {hex(id(x))}):
+           ob_refcnt: Py_ssize_t = 2
+           ob_type: *PyTypeObject = &[list]
+           ob_size: Py_ssize_t = 2
+           ob_item: **PyObject = &[&[1], &['A']]
+           allocated: c_long = 2
+        """
     )
