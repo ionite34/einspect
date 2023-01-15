ee60585403046a93d662f10fc55bfd3660e0efb9
ionite34 <dev@ionite.io>
2023-01-15 18:59:03 +0800

    Version bump to v0.4.4

--- src/einspect/__init__.py
+++ src/einspect/__init__.py
@@ -9,6 +9,6 @@ from einspect.views.view_type import impl
 
 __all__ = ("view", "unsafe", "impl", "orig")
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 unsafe: ContextManager[None] = global_unsafe
