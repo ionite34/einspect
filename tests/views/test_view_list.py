dc21c1ab04c9e48132c5f10366c6f9bbb316fe0d
ionite34 <dev@ionite.io>
2023-01-16 13:16:54 +0800

    Add setitem and attrs test for ListView

--- tests/views/test_view_list.py
+++ tests/views/test_view_list.py
@@ -15,6 +15,14 @@ class TestListView(TestView):
     def get_obj(self):
         return [400, 500, 600]
 
+    def test_attrs(self):
+        obj = [1, 2, 3]
+        v = self.view_type(obj)
+        assert v.size == len(obj)
+        assert v.allocated == 4
+        for i in range(3):
+            assert v.item[i].contents.into_object() is obj[i]
+
     def test_sized(self):
         obj = self.get_obj()
         orig_len = len(obj)
@@ -53,3 +61,10 @@ class TestListView(TestView):
         assert obj == [10, "hi", "abc"]
         v[:] = ("A", "B")
         assert obj == ["A", "B"]
+
+    def test_setitem_error(self):
+        obj = [1, 2, 3]
+        v = self.view_type(obj)
+        # Cannot set slice with step
+        with pytest.raises(ValueError):
+            v[1:2:2] = [1, 2]
