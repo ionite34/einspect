2fbafd5220b811955e301fb1c3edbf01becc2cd8
ionite34 <dev@ionite.io>
2023-01-17 13:04:10 +0800

    Add additional DictView tests

--- tests/views/test_view_dict.py
+++ tests/views/test_view_dict.py
@@ -1,7 +1,5 @@
 import pytest
 
-from einspect.structs import PyDictObject
-from einspect.views.factory import view
 from einspect.views.view_dict import DictView
 from tests.views.test_view_base import TestView
 
@@ -18,4 +16,41 @@ class TestDictView(TestView):
         v = self.view_type(obj)
         assert v.type is dict
         assert v.used == len(obj)
-        # assert v.ma_keys.contents == 0
+        assert len(v) == len(obj)
+
+    def test_iter(self):
+        d = {"a": 1, "b": 2}
+        v = self.view_type(d)
+        assert list(v) == list(d)
+        for a, b in zip(v, d):
+            assert a == b
+
+    def test_getitem(self):
+        obj = {"a": 1, "b": 2}
+        v = self.view_type(obj)
+        assert v["a"] == 1
+        assert v["b"] == 2
+        with pytest.raises(KeyError):
+            assert not v["c"]
+
+    def test_setitem(self):
+        obj = {"a": 1, "b": 2}
+        v = self.view_type(obj)
+        v["a"] = 100
+        assert v["a"] == 100
+        assert obj["a"] == 100
+        assert obj == {"a": 100, "b": 2}
+        v["c"] = 3
+        assert obj == {"a": 100, "b": 2, "c": 3}
+
+    def test_delitem(self):
+        obj = {"a": 1, "b": 2}
+        v = self.view_type(obj)
+        del v["a"]
+        assert obj == {"b": 2}
+
+        with pytest.raises(KeyError):
+            del v["a"]
+
+        with pytest.raises(KeyError):
+            del v["not_exists"]
