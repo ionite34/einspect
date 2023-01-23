c14b2abc1d9e1c4fbda4dcb69c073e97d35194f3
ionite34 <dev@ionite.io>
2023-01-23 16:44:11 -0500

    Add _format_fields_ for MappingProxyObject

--- src/einspect/structs/mapping_proxy.py
+++ src/einspect/structs/mapping_proxy.py
@@ -5,9 +5,11 @@ from typing import TypeVar
 
 from einspect.structs.deco import struct
 from einspect.structs.py_dict import PyDictObject
-from einspect.structs.py_object import PyObject
+from einspect.structs.py_object import Fields, PyObject
+from einspect.types import ptr
 
 __all__ = ("MappingProxyObject",)
+
 
 _KT = TypeVar("_KT")
 _VT_co = TypeVar("_VT_co", covariant=True)
@@ -21,7 +23,13 @@ class MappingProxyObject(PyObject[MappingProxyType, _KT, _VT_co]):
     https://github.com/python/cpython/blob/3.11/Objects/descrobject.c#L1027-L1030
     """
 
-    mapping: PyDictObject[_KT, _VT_co]
+    mapping: ptr[PyDictObject[_KT, _VT_co]]
+
+    def _format_fields_(self) -> Fields:
+        return {
+            **super()._format_fields_(),
+            "mapping": "*PyDictObject",
+        }
 
     @classmethod
     def from_object(
