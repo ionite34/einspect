96722300b4221cc6d225b0146a6d6572753fb41a
ionite34 <dev@ionite.io>
2023-01-24 15:53:47 -0500

    Improve error phrasing of _check_move

--- src/einspect/views/moves.py
+++ src/einspect/views/moves.py
@@ -12,10 +12,10 @@ if TYPE_CHECKING:
 
 def _check_move(dst: View, src: View) -> None:
     """
-    Check if the string can be resized to `target` size.
+    Check if a memory from `dst` to `src` is safe.
 
     Raises:
-        UnsafeError: If the string cannot be resized, and not in an unsafe context.
+        UnsafeError: If the move is unsafe.
     """
     # If non-gc type into gc type, always unsafe
     if not src.is_gc() and dst.is_gc():
@@ -38,16 +38,13 @@ def _check_move(dst: View, src: View) -> None:
     if (src_dict := src._pyobject.instance_dict()) is not None:
         src_offset = addressof(src_dict) - src._pyobject.address
         # If negative, it must match dst_offset
-        if src_offset < 0 and src_offset != dst_offset:
-            raise UnsafeError(
-                f"Move of instance dict from {src.type.__name__!r} to {dst.type.__name__!r}"
-                " requires an unsafe context."
-            )
+        neg_unsafe = src_offset < 0 and src_offset != dst_offset
         # Otherwise, needs to be within mem_allocated
-        if src_offset > 0 and (src_offset + PTR_SIZE) > dst_allocated:
+        pos_unsafe = src_offset > 0 and (src_offset + PTR_SIZE) > dst_allocated
+        if neg_unsafe or pos_unsafe:
             raise UnsafeError(
-                f"Move of instance dict from {src.type.__name__!r} to {dst.type.__name__!r}"
-                " requires an unsafe context."
+                f"memory move of instance dict at offset {src_offset} from {src.type.__name__!r} "
+                f"to {dst.type.__name__!r} is out of bounds. Enter an unsafe context to allow this."
             )
 
     # Check if src mem_size can fit into dst mem_allocated
