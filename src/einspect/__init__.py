383f90e1c9753ec933a4f931dabcfe4eac84eff3
ionite34 <dev@ionite.io>
2023-01-25 14:05:05 -0500

    Version bump to v0.5.1

--- src/einspect/__init__.py
+++ src/einspect/__init__.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import ContextManager
 
-# Runtime patches
 import einspect._patch
 from einspect.type_orig import orig
 from einspect.types import NULL, ptr
@@ -10,10 +9,11 @@ from einspect.views.factory import view
 from einspect.views.unsafe import global_unsafe
 from einspect.views.view_type import impl
 
+# Runtime patches
 einspect._patch.run()
 
 __all__ = ("view", "unsafe", "impl", "orig", "ptr", "NULL")
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 unsafe: ContextManager[None] = global_unsafe
