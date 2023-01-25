85769a9587f03c50204e1e5739d870b8835d81f0
ionite34 <dev@ionite.io>
2023-01-25 08:44:59 -0500

    Added StrView tests

--- tests/views/test_view_str.py
+++ tests/views/test_view_str.py
@@ -1,9 +1,12 @@
+from ast import literal_eval
+from contextlib import ExitStack
 from uuid import uuid4
 
 import pytest
 
 from einspect import view
-from einspect.structs.py_unicode import Kind, State
+from einspect.errors import UnsafeError
+from einspect.structs.py_unicode import Kind, PyCompactUnicodeObject, State
 from einspect.views.view_str import StrView
 from tests.views.test_view_base import TestView
 
@@ -13,17 +16,29 @@ class TestStrView(TestView):
     obj_type = str
 
     def get_obj(self) -> str:
-        return "2fe7b604-9664-41e6-9af0-9b472864bfc8"
+        return literal_eval("'2fe7b604-9664-41e6-9af0-9b472864bfc8'")
 
-    def test_properties(self):
+    def test_unicode(self) -> None:
+        text = literal_eval("'🤔'")
+        v = view(text)
+        assert v.buffer[0] == ord("🤔")
+        assert isinstance(v._pyobject, PyCompactUnicodeObject)
+
+    def test_properties(self) -> None:
         text = self.get_obj()
-        str_view = view(text)
-        assert str_view.length == len(text)
+        v = view(text)
+        assert v.length == len(text)
         expected_hash = hash(text)
-        assert str_view.hash == expected_hash
-        assert str_view.kind == Kind.PyUnicode_1BYTE
+        assert v.hash == expected_hash
+        assert v.kind == Kind.PyUnicode_1BYTE
 
-    def test_intern(self):
+    def test_unsafe_resize(self) -> None:
+        text = "abc"
+        v = view(text)
+        with pytest.raises(UnsafeError):
+            v.append("x" * 64)
+
+    def test_intern(self) -> None:
         text = self.get_obj()
         str_view = view(text)
         # Module level literal should be not interned
@@ -32,16 +47,45 @@ class TestStrView(TestView):
         s = "hi"
         assert view(s).interned == State.INTERNED_MORTAL
 
-    def test_buffer(self):
+    def test_setters(self) -> None:
+        text = self.get_obj()
+        v = self.view_type(text)
+
+        with ExitStack() as stack, v.unsafe():
+            # Check -1 hash will reset the hash cache
+            v.hash = -1
+            assert v.hash == -1
+            hash(text)
+            assert v.hash == hash(text)
+            assert v.hash != -1
+
+            orig_len = len(text)
+            assert v.length == orig_len
+            stack.callback(lambda: setattr(v, "length", orig_len))
+            v.length = orig_len // 2
+            assert len(text) == orig_len // 2
+
+            assert v.interned == State.NOT_INTERNED
+            stack.callback(lambda: setattr(v, "interned", State.NOT_INTERNED))
+            v.interned = State.INTERNED_MORTAL
+            assert v.interned == State.INTERNED_MORTAL
+
+            assert v.kind == Kind.PyUnicode_1BYTE
+            stack.callback(lambda: setattr(v, "kind", Kind.PyUnicode_1BYTE))
+            v.kind = Kind.PyUnicode_2BYTE
+            assert v.kind == Kind.PyUnicode_2BYTE
+
+    def test_buffer(self) -> None:
         text = self.get_obj()
         str_view = view(text)
         assert str_view.buffer[:] == text.encode("ascii")
 
 
-def test_str_setitem():
+def test_str_setitem() -> None:
     s = uuid4().hex
     orig_len = len(s)
     v = StrView(s)
+    assert v.interned == State.NOT_INTERNED
     v[0] = "1"
     v[-1] = "Z"
     assert s[0] == "1"
@@ -55,7 +99,7 @@ def test_str_setitem():
     assert len(s) == orig_len - 1
 
 
-def test_str_mutable_sequence():
+def test_str_mutable_sequence() -> None:
     s = uuid4().hex
     orig_len = len(s)
     last = s[-1]
@@ -64,3 +108,15 @@ def test_str_mutable_sequence():
     v.append("@")
     assert s[-1] == "@"
     assert len(s) == orig_len
+
+
+def test_str_remove() -> None:
+    s = literal_eval("'4ff4-e1219224-5462'")
+    assert s is not "4ff4-e1219224-5462"
+    v = StrView(s)
+    assert v.interned == State.NOT_INTERNED
+    v.remove("1219224")
+    assert s == "4ff4-e-5462"
+    # Removes only first instance
+    v.remove("-")
+    assert s == "4ff4e-5462"
