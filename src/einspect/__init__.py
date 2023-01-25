e155be9900c38045e91a5479a1ff64f172b01c09
ionite34 <dev@ionite.io>
2023-01-25 12:37:06 -0500

    Export NULL singleton to module level

--- src/einspect/__init__.py
+++ src/einspect/__init__.py
@@ -2,12 +2,17 @@ from __future__ import annotations
 
 from typing import ContextManager
 
+# Runtime patches
+import einspect._patch
 from einspect.type_orig import orig
+from einspect.types import NULL, ptr
 from einspect.views.factory import view
 from einspect.views.unsafe import global_unsafe
 from einspect.views.view_type import impl
 
-__all__ = ("view", "unsafe", "impl", "orig")
+einspect._patch.run()
+
+__all__ = ("view", "unsafe", "impl", "orig", "ptr", "NULL")
 
 __version__ = "0.5.0a1"
 
