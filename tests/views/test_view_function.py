201b0c288c100c63d6447e5dd63c4a56a4a19b01
ionite34 <dev@ionite.io>
2023-01-26 16:49:40 -0500

    Add tests for FunctionView

---
+++ tests/views/test_view_function.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from types import FunctionType
+
+import pytest
+
+from einspect.views import FunctionView
+from tests.views.test_view_base import TestView
+
+
+class TestFunctionView(TestView):
+    view_type = FunctionView
+    obj_type = FunctionType
+
+    def get_obj(self) -> FunctionType:
+        def foo():
+            return 1
+
+        return foo  # type: ignore
+
+    def test_attrs(self):
+        def foo():
+            return 1
+
+        v = self.view_type(foo)
+
+        assert v.globals == foo.__globals__
+        assert v.builtins == foo.__globals__["__builtins__"]
+        assert v.name == foo.__name__
+        assert v.qualname == foo.__qualname__
+        assert v.code == foo.__code__
+        assert v.defaults == foo.__defaults__
+        assert v.kwdefaults == foo.__kwdefaults__
+        assert v.closure == foo.__closure__
+        assert v.doc == foo.__doc__
+        assert v.module == foo.__module__
+
+        # Materialize dicts before testing
+        getattr(foo, "__dict__")
+        getattr(foo, "__annotations__")
+        assert v.dict == foo.__dict__
+        assert v.annotations == foo.__annotations__
+
+    def test_globals(self):
+        # noinspection PyUnresolvedReferences
+        def foo():
+            return stuff
+
+        with pytest.raises(NameError):
+            foo()
+
+        # inject globals
+        v = self.view_type(foo)
+        with v.unsafe():
+            v.globals = {"stuff": 123}
+
+        assert foo() == 123
