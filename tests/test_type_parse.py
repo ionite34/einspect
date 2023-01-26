539426f330e70227516f7c0e37dd4cc93489b087
ionite34 <dev@ionite.io>
2023-01-25 19:53:54 -0500

    Add some struct tests

--- tests/test_type_parse.py
+++ tests/test_type_parse.py
@@ -1,11 +1,12 @@
-from ctypes import POINTER, c_void_p
+from ctypes import POINTER, c_uint32, c_void_p
+from typing import Union
 
 import pytest
 
 from einspect.structs.deco import struct
 
 
-def test_struct():
+def test_struct() -> None:
     @struct
     class Foo:
         x: POINTER(c_void_p)
@@ -13,7 +14,7 @@ def test_struct():
     assert Foo
 
 
-def test_struct_pointer_error():
+def test_struct_pointer_error() -> None:
     with pytest.raises(TypeError):
 
         @struct
@@ -21,3 +22,11 @@ def test_struct_pointer_error():
             x: POINTER(str)  # type: ignore
 
         assert not Foo
+
+
+def test_struct_union():
+    @struct
+    class Foo:
+        x: Union[POINTER(c_void_p), POINTER(c_uint32)]
+
+    assert Foo.x._type_ == c_void_p
