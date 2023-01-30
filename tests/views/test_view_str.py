e89b0e64d2a5750da726c2a206f1906435c63ab4
Ionite <dev@ionite.io>
2023-01-30 11:47:27 -0500

    Add StrView sort tests

--- tests/views/test_view_str.py
+++ tests/views/test_view_str.py
@@ -119,6 +119,15 @@ def test_str_mutable_sequence() -> None:
     assert len(s) == orig_len
 
 
+# noinspection SpellCheckingInspection
+def test_tuple_sort():
+    s = literal_eval("'9a94-3f7109b7-888c'")
+    v = StrView(s)
+    assert v.interned == State.NOT_INTERNED
+    v.sort()
+    assert s == "--013477888999abcf"
+
+
 def test_str_remove() -> None:
     s = literal_eval("'4ff4-e1219224-5462'")
     v = StrView(s)
