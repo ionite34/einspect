4db995b6374fd5976cc9e1d25e62cb04ac0bdc3a
Ionite <dev@ionite.io>
2023-03-13 14:50:42 -0400

    Version bump to 0.5.13

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, str(Path(__file__).parents[2] / "src"))
 project = "einspect"
 copyright = "2023, Ionite"
 author = "Ionite"
-release = "v0.5.12"
+release = "v0.5.13"
 
 
 # -- General configuration ---------------------------------------------------
