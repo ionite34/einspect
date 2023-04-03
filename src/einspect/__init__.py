65837fbf3ca5e936741f45da25fabe49d7d86383
Ionite <dev@ionite.io>
2023-04-02 23:39:51 -0400

    Version bump to 0.5.16

--- src/einspect/__init__.py
+++ src/einspect/__init__.py
@@ -8,7 +8,7 @@ from einspect.views.unsafe import global_unsafe
 from einspect.views.view_type import impl
 
 __all__ = ("view", "unsafe", "impl", "orig", "ptr", "NULL", "__version__")
-__version__ = "0.5.15"
+__version__ = "0.5.16"
 
 unsafe = global_unsafe
 
